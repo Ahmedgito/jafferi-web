d1a90f2a3f0637960361888c10294e62c77dea20
yamaankhan20 <khanyamaan1@gmail.com>
2025-02-09 20:41:26 +0500

    private and public link updated

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,8 +3,8 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const PrivateRoute = ({ children }) => {
-    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-    return isAuthenticated ? children : <Navigate to="/signin" replace />;
+    const isAuthenticated = useSelector(state => state.auth?.isAuthenticated);
+    return isAuthenticated ? children : <Navigate to="/signin" />;
 };
 
 export default PrivateRoute;
--- src/routes/PublicRoute.jsx
+++ src/routes/PublicRoute.jsx
@@ -3,8 +3,8 @@ import { Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 const PublicRoute = ({ children }) => {
-    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
-    return isAuthenticated ? <Navigate to="/professionalnetwork " replace/> : children;
+    const isAuthenticated = useSelector(state => state.auth?.isAuthenticated);
+    return isAuthenticated ? <Navigate to="/professionalnetwork" replace /> : children;
 };
 
 export default PublicRoute;
