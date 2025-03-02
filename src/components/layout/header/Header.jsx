e4e6a324cccf203081c588f2f6cbd5ff39f544fe
Ahmedgito <mamooabid1@gmail.com>
2025-03-02 20:00:19 +0500

    Enhance Sign In button styles for improved visibility and responsiveness in Header component

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -62,7 +62,8 @@ const Header = () => {
             ) : (
                 <>
                   <Link to="/contact" className="text-white hover:text-gray-300">Contact Us</Link>
-                  <Link to="/signin" className="text-white hover:text-gray-300">Sign In</Link>
+                  <Link to="/signin" className="flex items-center gap-1 md:-mt-1 px-4 py-1 bg-[white] text-#003505 font-medium rounded-lg 
+             transition duration-300 hover:bg-[#003505] hover:text-[white] hover:border-2 hover:border-white cursor-pointer border-2 border-[#003505] shadow-md">Sign In</Link>
                 </>
             )}
           </div>
