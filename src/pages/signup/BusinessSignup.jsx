be4e3467e9f6ab130e697558795a1ff1f42e77dd
yamaankhan20 <khanyamaan1@gmail.com>
2025-03-14 16:59:33 +0500

    business form changes

--- src/pages/signup/BusinessSignup.jsx
+++ src/pages/signup/BusinessSignup.jsx
@@ -194,6 +194,7 @@ const Form2 = () => {
               className="border rounded-lg px-3 py-2 mt-1 mb-2 text-sm w-full focus:border-green-500 focus:ring-2 focus:ring-green-500"
               type="url"
               id="website_url"
+              value={formData.website_url}
             />
           </div>
 
